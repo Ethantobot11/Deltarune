45f3d7238dabe78bcb6f29b240b3a6413a711af1
Ethantobot <ethantobot7@gmail.com>
2026-07-26 00:30:30 +0200

    idk what im doing

--- source/Main.hx
+++ source/Main.hx
@@ -1,22 +1,99 @@
 package;
 
-import openfl.display.Sprite;
+import flixel.FlxG;
 import flixel.FlxGame;
+import flixel.FlxState;
+import openfl.Lib;
+import openfl.display.FPS;
+import openfl.display.Sprite;
+import openfl.events.Event;
+import openfl.display.StageScaleMode;
+
+#if windows
+@:buildXml('
+<target id="haxe">
+	<lib name="wininet.lib" if="windows" />
+	<lib name="dwmapi.lib" if="windows" />
+</target>
+')
+@:cppFileCode('
+#include <windows.h>
+#include <winuser.h>
+#pragma comment(lib, "Shell32.lib")
+extern "C" HRESULT WINAPI SetCurrentProcessExplicitAppUserModelID(PCWSTR AppID);
+')
+#end
 
 class Main extends Sprite
 {
-    var gameWidth:Int = 320;
-    var gameHeight:Int = 240;
+	var gameWidth:Int = 1280; 
+	var gameHeight:Int = 720; 
+	var zoom:Float = 1; 
+	var initialState:Class<FlxState> = PlayState;
+	var framerate:Int = 60; 
+	var skipSplash:Bool = true; 
+	var startFullscreen:Bool = false; 
+	public static var fpsVar:FPS;
 
-    var initialState:Class<flixel.FlxState> = PlayState;
+	public static function main():Void
+	{
+		Lib.current.addChild(new Main());
+		#if cpp
+		cpp.NativeGc.enable(true);
+		#elseif hl
+		hl.Gc.enable(true);
+		#end
+	}
 
-    var framerate:Int = #if desktop 60 #elseif mobile 30 #end;
-    var skipSplash:Bool = true;
-    var startFullscreen:Bool = false;
+	public function new()
+	{
+		#if windows
+		untyped __cpp__("SetProcessDPIAware();");
+		#end
 
-    public function new()
-    {
-        super();
-        addChild(new FlxGame(gameWidth, gameHeight, initialState, framerate, framerate, skipSplash, startFullscreen));
-    }
+		super();
+
+		if (stage != null)
+		{
+			init();
+		}
+		else
+		{
+			addEventListener(Event.ADDED_TO_STAGE, init);
+		}
+	}
+
+	private function init(?E:Event):Void	
+	{
+		if (hasEventListener(Event.ADDED_TO_STAGE))
+		{
+			removeEventListener(Event.ADDED_TO_STAGE, init);
+		}
+
+		setupGame();
+	}
+
+	private function setupGame():Void
+	{
+		addChild(new FlxGame(
+			gameWidth,
+			gameHeight,
+			initialState,
+			framerate,
+			framerate,
+			skipSplash,
+			startFullscreen
+		));
+
+		fpsVar = new FPS(10, 3, 0xFFFFFF);
+		addChild(fpsVar);
+		
+		Lib.current.stage.align = "tl";
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+
+		#if html5
+		FlxG.autoPause = false;
+		FlxG.mouse.visible = false;
+		#end
+	}
 }

040ee64e4892429d43eaab7712e08746328317b7
Ethantobot <ethantobot7@gmail.com>
2026-08-23 03:45:26 +0200

    Add imports for Flixel classes in import.hx

---
+++ source/import.hx
@@ -0,0 +1,17 @@
+#if !macro
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxEase;
+import flixel.system.FlxSound;
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+import flixel.graphics.frames.FlxAtlasFrames;
+#end

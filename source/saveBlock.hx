c040c306fc3fba65602f429ce9f1fd109e11b2fa
Ethantobot <ethantobot7@gmail.com>
2026-08-23 03:06:37 +0200

    Update saveBlock.hx

--- source/saveBlock.hx
+++ source/saveBlock.hx
@@ -1,1 +1,11 @@
+import flixel.FlxSprite;
 
+class saveBlock extends FlxSprite
+{
+    public var duh:FlxSprite;
+  
+    public function create()
+    {
+      duh.makeGraphic(0, 0, 0xFF222244);
+    }
+}

f4a45f6e149f6d2974e0186c7784411e9e4207b4
Ethantobot <ethantobot7@gmail.com>
2026-08-23 04:14:56 +0200

    Update import.hx

--- source/import.hx
+++ source/import.hx
@@ -1,4 +1,18 @@
 #if !macro
+
+// Android
+#if android
+import android.Tools as AndroidTools;
+import android.Settings as AndroidSettings;
+import android.widget.Toast as AndroidToast;
+import android.content.Context as AndroidContext;
+import android.Permissions as AndroidPermissions;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Environment as AndroidEnvironment;
+import android.os.BatteryManager as AndroidBatteryManager;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+#end
+
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxCamera;

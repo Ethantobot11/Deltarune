ae6b70c06a18307da35c1aed41792de4db7e3ded
Ethantobot <ethantobot7@gmail.com>
2026-08-23 03:17:14 +0200

    Change graphic size in SaveBlock constructor

--- source/SaveBlock.hx
+++ source/SaveBlock.hx
@@ -5,7 +5,7 @@ class SaveBlock extends FlxSprite
     public function new(x:Float, y:Float)
     {
         super(x, y);
-        makeGraphic(0, 0, 0xFF222244);
+        makeGraphic(32, 32, 0xFF222244);
         solid = true;
         immovable = true;
     }
